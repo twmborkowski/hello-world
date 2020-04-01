1f4f1dd27984a123a76fb5187716753401ae19c5
twmborkowski <twmborkowski@gmail.com>
2020-04-01 20:35:13 +0200

    Hello world w Java

---
+++ Hello_world.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Hello_world
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Witaj! Dzisiaj jest: ");
+    System.out.println(new Date());
+  }
+}
