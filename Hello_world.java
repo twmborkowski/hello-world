e5445106b5d8be0885b687e59fe9b11685ce1f2a
twmborkowski <twmborkowski@gmail.com>
2020-04-02 18:35:14 +0200

    Dalszy etap nauki 02.04.2020

--- Hello_world.java
+++ Hello_world.java
@@ -1,10 +1,23 @@
+//============================
+//=========LIBRARIES============
+//============================
+//import java.lang.*; //default
 import java.util.*;
 
+//============================
+//=========PROGRAM============
 public class Hello_world
 {
+  //========MAIN FUNCTION========
   public static void main(String[] args)
   {
     System.out.println("Witaj! Dzisiaj jest: ");
     System.out.println(new Date());
+    System.getProperties().list(System.out);
+    //informacje o środowisku wykonania programu
+    System.out.println(System.getProperty("user.name"));
+    //dostęp do nazwy użytkownika
+    System.out.println(System.getProperty("java.library.path"));
+    //dostęp do zmiennej środowiskowej PATH
   }
 }
